308a01c70187254e08d48ac415083c6bafedfc8a
Fahad Ali <115652088+CodeWithFadii@users.noreply.github.com>
2025-03-12 21:05:38 +0500

    posts CRUD implemented

--- app/main.py
+++ app/main.py
@@ -14,6 +14,10 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+def find_post(id):
+    return next(filter(lambda x: x["id"] == id, my_posts), None)
+
+
 my_posts = [
     {"title": "This is a title 1", "content": "This is a content 1", "id": 1},
     {"title": "This is a title 2", "content": "This is a content 2", "id": 2},
@@ -40,10 +44,35 @@ def create_post(post: Post):
 
 @app.get("/post/{id}")
 def get_post(id: int):
-    post = next(filter(lambda x: x["id"] == id, my_posts), None)
+    post = find_post(id=id)
     if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Their is not post with id: {id}",
         )
     return {"data": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    post = find_post(id=id)
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Their is not post with id: {id}",
+        )
+    my_posts.remove(post) @ app.delete(
+        "/posts/{id}", status_code=status.HTTP_204_NO_CONTENT
+    )
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post_data: Post):
+    post = find_post(id=id)
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Their is not post with id: {id}",
+        )
+    post.update(post_data)
+    return {"message": "Post updated successfully"}
