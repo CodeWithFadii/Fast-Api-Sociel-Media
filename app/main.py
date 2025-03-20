adfcde1f1d3bcdd8e7e34db54a4ab5cc29e0d178
Fahad Ali <115652088+CodeWithFadii@users.noreply.github.com>
2025-03-20 09:52:45 +0500

    create user added

--- app/main.py
+++ app/main.py
@@ -113,3 +113,21 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+# Getting a user by ID
+@app.post(
+    "/user", status_code=status.HTTP_201_CREATED, response_model=schemas.UserCreate
+)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    user_query = db.query(models.User).filter(models.User.email == user.email)
+    if user_query.first():
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"Email already exists",
+        )
+    new_user = models.User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
